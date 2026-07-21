db34cd0ffd7a5da080e4785bf7620da58cfc56e0
Eliot Muir <eliot.muir@interfaceware.com>
2026-07-21 12:17:39 -0500

    Update

--- BAS/BASglob.h
+++ BAS/BASglob.h
@@ -1,13 +1,13 @@
-#ifndef _BAS_GLOB_H__
-#define _BAS_GLOB_H__
+#pragma once
 //-------------------------------------------------------
-// Copyright (C) 2021 Eliot Muir.  All Rights Reserved
+// Copyright (C) Forever Eliot Muir.  All Rights Reserved
 //
 // BASglob
 // 
 // Global matching function.
+//
+// Supports expressions like 
 //-------------------------------------------------------
 
 bool BASglobMatch(const char* pTarget, const char* pPattern);
 
-#endif
