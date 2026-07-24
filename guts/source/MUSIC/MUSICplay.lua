208416ca0ab9ad5cd28e695a9257092496c187a8
Eliot Muir <eliot.muir@interfaceware.com>
2026-07-24 14:56:57 -0500

    Update

--- guts/source/MUSIC/MUSICplay.lua
+++ guts/source/MUSIC/MUSICplay.lua
@@ -3,7 +3,7 @@ require "URL/URLopen"
 function MUSICplay(Args)
    COL_VAR(Args)
    
-   local Url = "https://open.spotify.com/search/query="..table.concat(Args, " ", 2);
+   local Url = "https://open.spotify.com/search/query="..table.concat(Args, " ", 3);
    COL_VAR(Url)
    URLopen(Url);
 end
--- guts/source/main/main.lua
+++ guts/source/main/main.lua
@@ -3,8 +3,20 @@ require "FUNC/FUNCsplit"
 require "FUNC/FUNCluaCppExtension"
 require "MUSIC/MUSICplay"
 
-  -- FUNCsplit(Arg[2]);
+local Commands={
+  ["music:play"]=MUSICplay,
+  ["add:clua"]=FUNCluaCppExtension,
+  ["code:function:split"]=FUNCsplit
+}
+
 function main(Arg)
-   FUNCluaCppExtension(Arg[2]);
-   --MUSICplay(Arg)
+   COL_VAR(Arg)
+   if not Commands[Arg[2]] then
+      print "Please use a supported command:"
+      for K, _ in pairs(Commands) do
+         print("   "..K);
+      end
+      return;
+   end
+   Commands[Arg[2]](Arg);
 end
